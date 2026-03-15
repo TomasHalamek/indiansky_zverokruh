92780f05d3bcd4b7eee88309c3a703418d9ce008
Tomas Halamek <halamek.tomas@windowslive.com>
2026-03-15 21:01:21 +0100

    Příprava na hlavní grid

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalendář PLUS - Indiánské znamení zvěrokruhu</title>
+    <title>Kalendář PLUS - Indiánský zvěrokruh</title>
     <!-- Style -->
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/indiansky_zverokruh/assets/css/style.css">
     <!-- FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,15 +16,30 @@
 <body>
     <header></header>
     <main>
-        <!-- TITLE -->
-        <section class="wrapper-title">
-            <img src="/assets/images/feather_left.webp" alt="Levé pírko nápisu"
-                loading="lazy" decoding="async">
-            <H1>Indiánské znamení</H1>
-            <img src="/assets/images/feather_right.webp" alt="Pravé pírko nápisu"
-                loading="lazy" decoding="async">
-        </section>
+        <div class="indian-main-container">
+            <!-- TITLE -->
+            <section class="wrapper-title">
+                <img src="/indiansky_zverokruh/assets/images/feather_left.webp" alt="Levé pírko nápisu"
+                    width="120" height="47"
+                    loading="lazy" decoding="async">
+                <h1>Indiánské znamení</h1>
+                <img src="/indiansky_zverokruh/assets/images/feather_right.webp" alt="Pravé pírko nápisu"
+                    width="120" height="47"
+                    loading="lazy" decoding="async">
+            </section>
 
+            <!-- Introduction -->
+            <section class="introductory-text"></section>
+
+            <!-- INPUT DATE -->
+            <section class="input-date"></section>
+
+            <!-- RESULT CARD -->
+            <section class="result-card"></section>
+
+            <!-- GRID ZODIAC -->
+            <section class="grid-zodiac"git ></section>
+        </div>
     </main>
     <footer></footer>
 </body>
