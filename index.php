5f040582d082326ac4a2ced949cc0964f01e3d57
Tomáš Halámek <halamek.tomas@windowslive.com>
2026-03-15 06:41:27 +0100

    První propojení projektu

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalendář PLUS - Indiánské znamení zvěrokruhu</title>
+    <!-- Style -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <!-- TITLE -->
+        <section class="wrapper-title">
+            <img src="/assets/images/feather_left.webp" alt="Levé pírko nápisu"
+                loading="lazy" decoding="async">
+            <H1>Indiánské znamení</H1>
+            <img src="/assets/images/feather_right.webp" alt="Pravé pírko nápisu"
+                loading="lazy" decoding="async">
+        </section>
+
+    </main>
+    <footer></footer>
+</body>
+
+</html>
